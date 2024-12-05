bb6abefbe1781e8e5ce3835eb488871f87a09f7a
benjiblas11 <153689062+benjiblas11@users.noreply.github.com>
2024-12-05 09:48:24 -0700

    database connection attempt 3

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.use(express.urlencoded( {extended: true} ));
 const knex = require("knex") ({
   client : "pg",
   connection : {
-  host : process.env.RDS_HOSTNAME || "localhost",
-  user : process.env.RDS_USERNAME || "postgres",
-  password : process.env.RDS_PASSWORD || "Majorasmask11.",
+  host : process.env.RDS_HOSTNAME || "awseb-e-dcpssqafyh-stack-awsebrdsdatabase-ofssl7nxdyot.cn6220qmsuba.us-east-1.rds.amazonaws.com",
+  user : process.env.RDS_USERNAME || "ebroot",
+  password : process.env.RDS_PASSWORD || "iloveintex",
   database : process.env.RDS_DB_NAME || "ebdb",
   port : process.env.RDS_PORT || 5432,
   ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false  // Fixed line
